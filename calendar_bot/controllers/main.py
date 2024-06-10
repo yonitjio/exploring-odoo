91cb8b652cc918da999c30313c22a5ca2a25223c
Yoni <yoni.tjio@gmail.com>
2024-06-10 09:55:43 +0700

    Fix typo

--- calendar_bot/controllers/main.py
+++ calendar_bot/controllers/main.py
@@ -6,5 +6,5 @@ from odoo.http import request
 class CalendarBotController(http.Controller):
     @http.route('/calendar_bot/query', type='json', auth='user')
     def query(self, thread_id, thread_model, author_id, query):
-        AdvAiBot = request.env["mail.bot.calendar"]
-        AdvAiBot.query(thread_id, thread_model, author_id, query)
+        CalendarAiBot = request.env["mail.bot.calendar"]
+        CalendarAiBot.query(thread_id, thread_model, author_id, query)
