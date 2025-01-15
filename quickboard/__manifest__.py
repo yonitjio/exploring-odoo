c1dc515109a84cdc9b31e3e8ff0a088d744e6f0d
Burnaviour <meharmuzafar760@gmail.com>
2025-01-15 17:40:03 +0500

    [FIX]: external dependencies added for quickboard to fix installation error

--- quickboard/__manifest__.py
+++ quickboard/__manifest__.py
@@ -1,36 +1,34 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Quickboard",
-    'summary': """Quickboard is a simple and easy to use dashboard powered with AI.""",
-    'description': """
+    "name": "Quickboard",
+    "summary": """Quickboard is a simple and easy to use dashboard powered with AI.""",
+    "description": """
         Quickboard is a simple and easy to use dashboard powered with AI.
     """,
-    'author': "Yoni Tjio",
-    'category': 'Productivity',
-    'version': '18.0.1.0.0',
-    'depends': ['web'],
-    'data': [
-        'security/quickboard_security.xml',
-        'security/ir.model.access.csv',
-        'views/quickboard_views.xml',
-        'views/quickboard_item_views.xml',
-        'wizard/quickboard_generator_views.xml'
+    "author": "Yoni Tjio",
+    "category": "Productivity",
+    "version": "18.0.1.0.0",
+    "depends": ["web"],
+    "data": [
+        "security/quickboard_security.xml",
+        "security/ir.model.access.csv",
+        "views/quickboard_views.xml",
+        "views/quickboard_item_views.xml",
+        "wizard/quickboard_generator_views.xml",
     ],
-    'assets': {
-         "web.assets_backend": [
-            "quickboard/static/src/**/*",
-            ("remove", "quickboard/static/src/quickboard/**/*")
-        ],
+    "assets": {
+        "web.assets_backend": ["quickboard/static/src/**/*", ("remove", "quickboard/static/src/quickboard/**/*")],
         "quickboard.assets": [
-            ('include', "web.chartjs_lib"),
+            ("include", "web.chartjs_lib"),
             "quickboard/static/lib/gridstack/*",
             "quickboard/static/lib/spinjs/*",
             "quickboard/static/src/quickboard/**/*",
             "quickboard/static/src/css/**/*",
         ],
-   },
-    "license":"Other proprietary",
+    },
+    "external_dependencies": {"python": ["pandas", "jsonschema", "autogen", "prettytable"]},
+    "license": "Other proprietary",
     "application": True,
     "installable": True,
-    "auto_install": False
+    "auto_install": False,
 }
