b54b2c09a2f0219c8495bd6d49261a1441c22ff5
Yoni <22361729+yonitjio@users.noreply.github.com>
2026-01-04 09:11:14 +0700

    Remove data_faker modules

--- data_faker_sales/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-© 2025 Yoni
-This software is experimental and provided "as-is".
-No guarantees, warranties, or liability are assumed.
-See the LICENSE file included with this software for full details.
-"""
-
-from . import models
